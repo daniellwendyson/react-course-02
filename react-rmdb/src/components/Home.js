9736ec2656fcd006b082539589d8c9b865b25a76
Daniell Wendyson <daniellwendyson@gmail.com>
2023-11-05 21:45:45 -0300

    Add thumb component and thumb styles

--- react-rmdb/src/components/Home.js
+++ react-rmdb/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 
 
@@ -8,6 +8,8 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 import HeroImage from './HeroImage';
 import Grid from './Grid';
+import Thumb from './Thumb';
+import Spinner from './Spinner';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -30,10 +32,20 @@ const Home = () => {
             ) : null}
             <Grid header='Popular Movies'>
                 {state.results.map(movie => (
-                    <div key={movie.id}>{movie.title}</div>
+                    <Thumb 
+                        key={movie.id}
+                        clickable
+                        image={
+                            movie.poster_path
+                                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                                : NoImage
+                        }
+                        movieId={movie.id}
+                    />
                 ))}
 
             </Grid>
+            <Spinner />
         </>
     );
 };
